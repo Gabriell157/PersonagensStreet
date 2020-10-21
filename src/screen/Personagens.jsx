cb4fd3dc5989b2b9b3a1bbbd5301023baa9171c1
gabriel <gabriel05@gmail.com>
2020-10-21 18:14:48 -0300

    lutinha

--- src/screen/Personagens.jsx
+++ src/screen/Personagens.jsx
@@ -47,37 +47,37 @@ function Paises() {
     return (
         <div>
             <p>Spain</p>
-            <img src="Spain" alt="Spain" />
+            <img src="https://static.todamateria.com.br/upload/es/pa/espanhabandeiraxx.jpg" alt="Spain" width="100" height="100" />
 
             <p>Russia</p>
-            <img src="Russia" alt="Russia" />
+            <img src="https://static.todamateria.com.br/upload/ba/nd/bandeirabb-0.jpg" alt="Russia" />
 
             <p>India </p>
-            <img src="India" alt="India" />
+            <img src="Indihttps://www.estudopratico.com.br/wp-content/uploads/2017/05/significado-da-bandeira-da-india.jpg" alt="India" width="100" height="100" />
 
             <p>China</p>
-            <img src="China" alt="China" />
+            <img src="https://www.significados.com.br/foto/bandeira-da-china.jpg" alt="China" width="100" height="100" />
 
             <p>Thailandia</p>
-            <img src="Thalandia" alt="Thailandia" />
+            <img src="Thttps://www.estudopratico.com.br/wp-content/uploads/2017/04/significado-da-bandeira-da-tailandia.jpg" alt="Thailandia" width="100" height="100" />
 
             <p>Japan</p>
-            <img src="Japan" alt="Japan" />
+            <img src="https://www.br.emb-japan.go.jp/cultura/bandeira.jpg" alt="Japan" width="100" height="100" />
 
             <p>Japan</p>
-            <img src="Japan" alt="Japan" />
+            <img src="https://www.br.emb-japan.go.jp/cultura/bandeira.jpg" alt="Japan" width="100" height="100" />
 
             <p>USA</p>
-            <img src="USA" alt="USA" />
+            <img src="https://image.freepik.com/vetores-gratis/ilustracao-de-bandeira-eua_53876-18165.jpg" alt="USA" width="100" height="100" />
 
             <p>USA</p>
-            <img src="USA" alt="USA" />
+            <img src="https://image.freepik.com/vetores-gratis/ilustracao-de-bandeira-eua_53876-18165.jpg" alt="USA" width="100" height="100" />
 
             <p>USA</p>
-            <img src="USA" alt="USA" />
+            <img src="https://image.freepik.com/vetores-gratis/ilustracao-de-bandeira-eua_53876-18165.jpg" alt="USA" width="100" height="100" />
 
             <p>Brasil</p>
-            <img src="Brasil" alt="Brasil" />
+            <img src="https://s1.static.brasilescola.uol.com.br/be/conteudo/images/2-bandeira-do-brasil.jpg" alt="Brasil" width="100" height="100" />
 
         </div>
     )
